5ae567bc6a0211ce1a633d983fc3090a6076f249
Hitinoviy <vovan.korchagin.1999@gmail.com>
2020-11-08 22:30:55 +0300

    inicial commit

---
+++ js/jsTest1.js
@@ -0,0 +1,10 @@
+// let age = prompt('Сколько тебе лет?', '');
+
+// if(age == null){
+//     alert('ОЙ все!');
+// }else{
+//     alert(`Тебе ${age} лет!`);}
+
+// let isBoss = confirm("Ты здесь главный?");
+
+// alert( isBoss );
