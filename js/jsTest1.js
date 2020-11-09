0796749b18e968df5955e19104a7876de2d7b73b
Hitinoviy <vovan.korchagin.1999@gmail.com>
2020-11-09 15:50:22 +0300

    inicial commit

--- js/jsTest1.js
+++ js/jsTest1.js
@@ -1,6 +1,6 @@
 // let age = prompt('Сколько тебе лет?', '');
 
-// if(age == null){
+// if(age == null || age == ''){
 //     alert('ОЙ все!');
 // }else{
 //     alert(`Тебе ${age} лет!`);}
@@ -8,3 +8,28 @@
 // let isBoss = confirm("Ты здесь главный?");
 
 // alert( isBoss );
+// let apples = "2";
+// let oranges = "3";
+// оба операнда предварительно преобразованы в числа
+// alert( +apples + +oranges );
+// alert( apples + oranges );
+// let age = prompt('Возраст?', 18);
+
+// let message = (age < 3) ? 'Здравствуй, малыш!' :
+//   (age < 18) ? 'Привет!' :
+//   (age < 100) ? 'Здравствуйте!' :
+//   'Какой необычный возраст!';
+
+// alert( message );
+
+// Одно и тоже  
+
+// if (age < 3) {
+//     message = 'Здравствуй, малыш!';
+//   } else if (age < 18) {
+//     message = 'Привет!';
+//   } else if (age < 100) {
+//     message = 'Здравствуйте!';
+//   } else {
+//     message = 'Какой необычный возраст!';
+//   }
